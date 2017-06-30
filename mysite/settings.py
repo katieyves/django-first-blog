1337f4c099129535be0bc4324695a92aca7547f1
Katie Yves <EkaterinaIV@users.noreply.github.com>
2017-06-30 23:48:17 +0300

    added hostname to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6^%o==)-j!f%cin6verngm8jpjohe*frgmmulmfg%h^y@j0m(*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['katieyves.pythonanywhere.com']
 
 
 # Application definition
